ec690abd03c3291644946559d0559b8d33d49ef2
Ivan Miljancic <ivan.miljancic@gmail.com>
2019-04-24 10:17:21 +0200

    Adjusted configs for database, application and vagrant

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=ucka.veleri.hr;dbname=bsvetlicic',
+    'username' => 'bsvetlicic',
+    'password' => '11',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
