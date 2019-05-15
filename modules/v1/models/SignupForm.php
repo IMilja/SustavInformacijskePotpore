fbf2f9dabef5efb49c8e063d90da0953cab906f2
Ivan Miljančić <ivan.miljancic@gmail.com>
2019-05-15 12:21:20 +0200

    Created signup/login, added required auth for REST, minor fixes
    Co-Authored-By: ToniO8 <toni4848@users.noreply.github.com>
    Co-Authored-By: bsvetlici <bsvetlici@users.noreply.github.com>

---
+++ modules/v1/models/SignupForm.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\modules\v1\models;
+
+use app\models\Korisnik;
+use app\models\Uloga;
+use yii\base\Model;
+
+
+class SignupForm extends Model
+{
+  public $username;
+  public $password;
+  public $email;
+  public $name;
+  public $surname;
+  public $phoneNumber;
+
+  public function signup()
+  {
+    $user = new Korisnik();
+
+    $user->Korisnicko_Ime = $this->username;
+    $user->Lozinka = $this->password;
+    $user->EMail = $this->email;
+    $user->Ime = $this->name;
+    $user->Prezime = $this->surname;
+    $user->Broj_Mobitela = $this->phoneNumber;
+    $user->ID_Uloge = Uloga::findOne(['Naziv_Uloge' => 'Civil'])->ID;
+
+    if ($user->validate(['Korisnicko_Ime', 'Lozinka', 'EMail', 'Ime', 'Prezime', 'Broj_Mobitela']) &&
+      $user->save(true,
+        ['Korisnicko_Ime', 'Lozinka', 'EMail', 'Ime', 'Prezime', 'Broj_Mobitela', 'accessToken', 'ID_Uloge'])) {
+      return null;
+    } else {
+      return $user->getErrors();
+    }
+  }
+}
