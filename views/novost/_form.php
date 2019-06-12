e1c638bdd4681cdc9e6c18be3d36a75ac34cd6c4
Ivan Miljančić <ivan.miljancic@gmail.com>
2019-06-12 07:59:08 +0200

    Minor adjustments to views/novost
    Co-Authored-By: bsvetlici <bsvetlici@users.noreply.github.com>
    Co-Authored-By: ToniO8 <toni4848@users.noreply.github.com>

--- views/novost/_form.php
+++ views/novost/_form.php
@@ -10,16 +10,16 @@ use yii\widgets\ActiveForm;
 
 <div class="novost-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+  <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'Opis')->textInput(['maxlength' => true]) ?>
+  <?= $form->field($model, 'Naslov')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'Naslov')->textarea(['maxlength' => true]) ?>
+  <?= $form->field($model, 'Opis')->textarea(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Spremi', ['class' => 'btn btn-success']) ?>
+      <?= Html::submitButton('Spremi', ['class' => 'btn btn-success']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+  <?php ActiveForm::end(); ?>
 
 </div>
